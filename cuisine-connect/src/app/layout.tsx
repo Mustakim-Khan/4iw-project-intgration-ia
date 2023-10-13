05a54de69d4d68cf2414ad6bf0aee2f9d1840df0
Moufid17 <moufid.mtr@gmail.com>
2023-10-13 12:04:41 +0200

    Set AppLayout

--- cuisine-connect/src/app/layout.tsx
+++ cuisine-connect/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import './globals.css'
 import * as React from 'react'
 import { Box, Sheet } from '@mui/joy'
 import Header from './components/Header'
+import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
--- cuisine-connect/src/app/page.js
+++ cuisine-connect/src/app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home(
   params, search
 ) {
-  return (sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+  return (
     <Box>
       <Sheet>
         Home page
