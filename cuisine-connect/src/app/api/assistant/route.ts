bcd04220e73696fe10df75b819e4bd15222bbd09
jeremdh62 <jdhaille@myges.fr>
2023-11-20 21:06:15 +0100

    Add chat history to API assistant route

--- cuisine-connect/src/app/api/assistant/route.ts
+++ cuisine-connect/src/app/api/assistant/route.ts
@@ -6,6 +6,13 @@ const schema = z.object({
    recipes : z.string()
 });
 
+const chat = [
+    {
+        role: "system",
+        content: "Tu es un chef étoilé au guide michelin ayant une 15aines d’années d’expérience dans le métier avec plusieurs concours culinaires gagnés à l’internationnal et tu dois aider les utilisateurs à trouver des recettes de cuisine en fonction de leurs besoins."
+    },
+];
+
 export async function POST(request: Request) {
   
         const body = await request.json();
@@ -14,20 +21,18 @@ export async function POST(request: Request) {
         const openai = new OpenAI({
             apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
         });
+
+        chat.push({
+            role: "user",
+            content: `${recipes}`
+        });
     
         const completions = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
-            messages: [
-                {
-                    role: "system",
-                    content: "Tu es un chef étoilé au guide michelin ayant une 15aines d’années d’expérience dans le métier avec plusieurs concours culinaires gagnés à l’internationnal et tu dois aider les utilisateurs à trouver des recettes de cuisine en fonction de leurs besoins."
-                },
-                {
-                    role: "user",
-                    content: `${recipes}`
-                }
-            ]
+            messages: chat as Array<any>,
         });
+
+        chat.push(completions.choices[0].message);        
     
         return NextResponse.json({
             result: completions.choices[0].message
