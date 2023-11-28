2c8efebda552248774b9b3c7e9d2251f0d134565
Mustakim-Khan <mustakimzidan@gmail.com>
2023-11-28 18:17:25 +0100

    all seeds work + new action for request

--- cuisine-connect/src/app/actions/recipes.ts
+++ cuisine-connect/src/app/actions/recipes.ts
@@ -5,3 +5,19 @@ import {prisma} from "../lib/authOptions"
 export async function findAllRecipes() {
     return await prisma.recipe.findMany();
 }
+
+export async function findAllRecipesForRequest() {
+    return await prisma.recipe.findMany({
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            keywords: true,
+            time: true,
+            ingredients: true,
+            steps: false,
+            nutriments: true,
+            nutriScore: true,
+        }
+    });
+}
--- cuisine-connect/src/app/api/search/route.ts
+++ cuisine-connect/src/app/api/search/route.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 import { OpenAI } from "openai";
 import { NextResponse } from "next/server";
 import { currentUser } from "../../actions/users";
-import { findAllRecipes } from "../../actions/recipes";
+import { findAllRecipes, findAllRecipesForRequest } from "../../actions/recipes";
+import { sys } from "typescript";
 
 const postSchema = z.object({
   search: z.string(),
@@ -10,7 +11,7 @@ const postSchema = z.object({
 
 export async function POST(request: Request) {
   const user = await currentUser();
-  const recipes = await findAllRecipes();
+  const recipes = await findAllRecipesForRequest();
 
   const body = await request.json();
   const { search } = postSchema.parse(body);
@@ -21,6 +22,7 @@ export async function POST(request: Request) {
 
   const systemContent = `Tu es un cuistot de grande renommé.
 Ton job est de répondre aux questions de cuisine qui te sont posées.
+Il est interdit pour toi des recherches sur internet.
 À partir de maintenant, dès que tu recevras une ingrédient de recette,
 tu renverras un tableau JSON de chaînes de caractères dans lequel tu renverra la liste des recettes qui correspondent à la recherche qui te sera donnée.
 pour chaque recette, tu renverras un objet json contenant le nom de la recette, une description et un temps estimé pour faire cuisiner le plat.
@@ -29,12 +31,15 @@ Le résultat final doit être cette forme zod :
 const schema = z.object({message: z.object({role: z.string(),content: z.string()})});
 Chaque recette doit être sous la forme :\{\"nom\": \"\",\"description\": \"\",\"temps\": \"\"},
 Donne le résultat de la recherche sous forme d'un tableau de string sans clé pour le premier object.
-Pour donner le résultat, tu dois te reposer sur l'objet 'recipes' que je te fournis. Donc les résultats doivent appartenir dans l'objet 'recipes' donné. Tu peut utilises les champs 'keywords' et 'title' pour filtrer les recettes en fonction de la recherche de l'utilisateur.\
+Pour donner le résultat, tu dois te reposer sur l'objet 'recipes' que je te fournis. Donc les résultats doivent obligatoirement provenir de l'objet 'recipes' que je te fournis. Si tu ne trouve pas de recettes dans l'object 'recettes' que je te fournis, envoie un tableau vide.
+Tu peut utilises les champs 'keywords' et 'title' pour filtrer les recettes en fonction de la recherche de l'utilisateur.
 Utilise également l'objet 'user' pour savoir qui est l'utilisateur qui te demande la recherche. En tenant compte de ses allergies dans le champ 'allergy' de l'objet 'user'.
 Voici l'objet recipes :
 ${JSON.stringify(recipes)}
 Voici l'objet user :
 ${JSON.stringify(user)}`;
+
+console.log("systemContent", systemContent);
 
   const completions = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
@@ -57,6 +62,8 @@ ${JSON.stringify(user)}`;
       },
     ],
   });
+
+  console.log("completions", completions.choices[0].message);
 
   return NextResponse.json({
     message: completions.choices[0].message as unknown as string,
